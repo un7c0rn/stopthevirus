243b0b9a0ca650f538bb2571531793271c0c2d36
un7c0rn <>
2020-03-23 14:59:05 -0700

    additional code reorganization.

--- game_engine/database.py
+++ game_engine/database.py
@@ -6,6 +6,7 @@ import json
 import time
 import logging
 import attr
+import engine_lib
 
 class DatabaseObject(object):
     # TODO(brandon): consider using SQL ORM bindings
@@ -43,6 +44,15 @@ class Challenge(DatabaseObject):
     start_timestamp: int = attr.ib()
     end_timestamp: int = attr.ib()
 
+@attr.s
+class ChallengeEntry(DatabaseObject):
+    id: Text = attr.ib()
+    likes: int = attr.ib()
+    views: int = attr.ib()
+    player_id: Text = attr.ib()
+    challenge_id: Text = attr.ib()
+    url: Text = attr.ib()
+
 class VotingReason(Enum):
     KICKOFF = 1
     WIN_IT_ALL = 2
@@ -54,6 +64,27 @@ class Vote(DatabaseObject):
     reason: VotingReason = attr.ib(default=VotingReason.KICKOFF)
 
 class Database(ABC):
+
+    def get_players(self, team: Team):
+        pass
+        
+    def get_teams_with_size(self, tribe: Tribe, team_size: int):
+        pass
+
+    def replace_tribe(self, tribe: Tribe, new_tribe: Tribe):
+        # updates tribe id for all members of tribe to new_tribe
+        pass
+
+    def get_next_challenge(self) -> Challenge:
+        # select oldest challenge that has not already been played
+        # mark challenge played in db
+        # TODO(brandon): gamedb interface get next challenge
+      return Challenge(id='1234',
+        name='Karaoke',
+        message='These are the challenge rules',
+        start_timestamp=engine_lib.get_unix_timestamp(),
+        end_timestamp=engine_lib.get_unix_timestamp() + 5)
+
     def create_player(self, player: Player):
         pass
 
