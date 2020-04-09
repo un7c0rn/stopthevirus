744bd3e1147441fae58eae3ab3e87b44230e8dc3
dzx11 <dzeifman2010@gmail.com>
2020-04-09 20:38:01 +0900

    Fix StartPage

--- react_front_end/src/pages/StartGamePage.js
+++ react_front_end/src/pages/StartGamePage.js
@@ -4,64 +4,35 @@ import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import React, { useRef } from "react";
 import { useParams } from "react-router-dom";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    "& > *": {
-      width: "100vw",
-      height: "25vh",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center"
-    }
-  },
-  title: {
-    margin: "1em 0",
-    fontSize: "1em"
-  },
-  form: {
-    display: "flex",
-    flexDirection: "column",
-    "& > *": {
-      margin: "1em 0"
-    }
-  }
-}));
+import ButtonAppBar from "./components/AppBar";
+import AppTitle from "./components/AppTitle";
+import Footer from "./components/Footer";
+import ChallengeInfo from "./components/ChallengeInfo";
+import ChallengeSubmit from "./components/ChallengeSubmit";
 
 export default function StartGamePage() {
+  const useStyles = makeStyles(theme => ({
+    root: {
+      background: theme.background,
+      color: "white",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "flex-start"
+    }
+  }));
+
   const classes = useStyles();
 
-  const inputRef = useRef();
-
-  const { phone } = useParams();
-
-  const { game } = useParams();
-
-  const submit = event => {
-    console.log("send to API endpoint");
-    console.log(inputRef.current.value);
-    console.log(phone);
-    console.log(game);
-  };
-
   return (
-    <div className={classes.root}>
-      <Paper square>
-        <form className={classes.form} noValidate autoComplete="off">
-          <TextField
-            id="outlined-basic"
-            label="Your TikTok Video Link"
-            variant="outlined"
-            inputRef={inputRef}
-          />
-          <Button variant="contained" onClick={submit}>
-            submit
-          </Button>
-        </form>
-      </Paper>
-    </div>
+    <>
+      <section className={classes.root}>
+        <ButtonAppBar />
+        <AppTitle />
+        <ChallengeInfo />
+        <ChallengeSubmit />
+        <Footer />
+      </section>
+    </>
   );
 }
