741e5cd21b4cdf0ddd60a20446255f79d02a196e
Bernard Baker <bernard.baker.me@gmail.com>
2020-04-11 13:16:27 +0100

    Updated tests.

--- react_front_end/src/App.test.js
+++ react_front_end/src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders progressbar", async () => {
+  const { getByRole } = render(<App />);
+  const element = getByRole(/progressbar/i);
+  expect(element).toBeInTheDocument();
 });
