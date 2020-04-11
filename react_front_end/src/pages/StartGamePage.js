746c51a461b15720e070a93b08a95b3008efbca4
dzx11 <dzeifman2010@gmail.com>
2020-04-12 03:27:12 +0900

    Fix routing and clean up code

--- react_front_end/src/pages/StartGamePage.js
+++ react_front_end/src/pages/StartGamePage.js
@@ -1,14 +1,8 @@
-import Button from "@material-ui/core/Button";
-import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import React, { useRef } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
 import ButtonAppBar from "./components/AppBar";
 import AppTitle from "./components/AppTitle";
 import Footer from "./components/Footer";
-import ChallengeInfo from "./components/ChallengeInfo";
-import ChallengeSubmit from "./components/ChallengeSubmit";
 import StartGamePrompt from "./components/StartGamePrompt";
 import StartGameInputs from "./components/StartGameInputs";
 
