1b9c423e233f97d25474f39e01fcc9422be231b0
dzx11 <dzeifman2010@gmail.com>
2020-04-15 03:13:31 +0900

    Add name input, phone input, submit tests (submit is failing)

--- react_front_end/src/__tests__/pages/components/StartGameInputs.js
+++ react_front_end/src/__tests__/pages/components/StartGameInputs.js
@@ -7,7 +7,7 @@ import StartGameInputs from "../../../pages/components/StartGameInputs";
 
 
 test("TikTok input", () => {
-  const testMessage = 'Test Message'
+  const testMessage = 'Test Message';
   render(<StartGameInputs />)
   fireEvent.change(screen.getByLabelText("Tik Tok"), {
     target: {value: testMessage},
@@ -16,7 +16,28 @@ test("TikTok input", () => {
 });
 
 test("Phone number input", async () => {
+  const testMessage = '+1 (23)';//this component will automatically add parenthesis
+  render(<StartGameInputs />)
+  fireEvent.change(screen.getByLabelText("SMS Phone Number"), {
+    target: {value: testMessage},
+  });
+  expect(screen.getByLabelText("SMS Phone Number").value).toEqual(testMessage);
 });
 
 test("Game name input", async () => {
+  const testMessage = 'Test Message';
+  render(<StartGameInputs />)
+  fireEvent.change(screen.getByLabelText("Your Game Name"), {
+    target: {value: testMessage},
+  });
+  expect(screen.getByLabelText("Your Game Name").value).toEqual(testMessage);
+});
+
+test("Phone number submit", async () => {
+  render(<StartGameInputs />)
+  //expect(screen.getByLabelText("SMS Phone Number").className).toNotContain("error");
+  fireEvent.change(screen.getByText("Start a game"), new MouseEvent('click'));
+
+  screen.debug(screen.getByLabelText("SMS Phone Number"))
+  expect(screen.getByLabelText("SMS Phone Number").className).toContain("error");
 });
