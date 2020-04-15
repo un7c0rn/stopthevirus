cb09c36d22758c51df7a7444c021597f574f3ee3
dzx11 <dzeifman2010@gmail.com>
2020-04-16 02:55:01 +0900

    Refactor to avoid code reuse

--- react_front_end/src/__tests__/pages/components/StartGameInputs.js
+++ react_front_end/src/__tests__/pages/components/StartGameInputs.js
@@ -2,110 +2,123 @@ import '@testing-library/jest-dom'
 import React from "react";
 import {render, fireEvent, screen} from '@testing-library/react'
 import StartGameInputs from "../../../pages/components/StartGameInputs";
-import { act } from 'react-dom/test-utils';
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 
 
+function tikTokLocater() {
+  return screen.getByLabelText("Tik Tok");
+}
 
+function phoneNumberLocator() {
+  return screen.getByLabelText("SMS Phone Number");
+}
+
+function startGameLocator() {
+  return screen.getByText("Start a game")
+}
+
+function gameNameLocator() {
+  return screen.getByLabelText("Your Game Name");
+}
 
 test("TikTok input", () => {
   const testMessage = 'Test Message';
   render(<StartGameInputs />)
-  fireEvent.change(screen.getByLabelText("Tik Tok"), {
+  fireEvent.change(tikTokLocater(), {
     target: {value: testMessage},
   });
-  expect(screen.getByLabelText("Tik Tok").value).toEqual(testMessage);
+  expect(tikTokLocater().value).toEqual(testMessage);
 });
 
 test("Phone number input", async () => {
   const testMessage = '+1 (23)';//this component will automatically add parenthesis
   render(<StartGameInputs />)
-  fireEvent.change(screen.getByLabelText("SMS Phone Number"), {
+  fireEvent.change(phoneNumberLocator(), {
     target: {value: testMessage},
   });
-  expect(screen.getByLabelText("SMS Phone Number").value).toEqual(testMessage);
+  expect(phoneNumberLocator().value).toEqual(testMessage);
 });
 
 test("Game name input", async () => {
   const testMessage = 'Test Message';
   render(<StartGameInputs />)
-  fireEvent.change(screen.getByLabelText("Your Game Name"), {
+  fireEvent.change(gameNameLocator(), {
     target: {value: testMessage},
   });
-  expect(screen.getByLabelText("Your Game Name").value).toEqual(testMessage);
+  expect(gameNameLocator().value).toEqual(testMessage);
 });
 
 test("Phone number submit invalid", async () => {
   render(<StartGameInputs />)
-  expect(screen.getByLabelText("SMS Phone Number")).toHaveAttribute("aria-invalid", "false");
-  fireEvent.click(screen.getByText("Start a game"));
-  expect(screen.getByLabelText("SMS Phone Number")).toHaveAttribute("aria-invalid", "true");
+  expect(phoneNumberLocator()).toHaveAttribute("aria-invalid", "false");
+  fireEvent.click(startGameLocator());
+  expect(phoneNumberLocator()).toHaveAttribute("aria-invalid", "true");
 });
 
 test("Phone number submit short invalid", async () => {
   render(<StartGameInputs />)
-  expect(screen.getByLabelText("SMS Phone Number")).toHaveAttribute("aria-invalid", "false");
+  expect(phoneNumberLocator()).toHaveAttribute("aria-invalid", "false");
 
   //enter invalid phone #
   const testMessage = '+1';
-  fireEvent.change(screen.getByLabelText("SMS Phone Number"), {
+  fireEvent.change(phoneNumberLocator(), {
     target: {value: testMessage},
   });
 
-  fireEvent.click(screen.getByText("Start a game"));
-  expect(screen.getByLabelText("SMS Phone Number")).toHaveAttribute("aria-invalid", "true");
+  fireEvent.click(startGameLocator());
+  expect(phoneNumberLocator()).toHaveAttribute("aria-invalid", "true");
 });
 
 test("Phone number submit valid", async () => {
   render(<StartGameInputs />)
-  expect(screen.getByLabelText("SMS Phone Number")).toHaveAttribute("aria-invalid", "false");
+  expect(phoneNumberLocator()).toHaveAttribute("aria-invalid", "false");
 
   //enter valid phone #
   const testMessage = '+1 (234) 567-8900';
-  fireEvent.change(screen.getByLabelText("SMS Phone Number"), {
+  fireEvent.change(phoneNumberLocator(), {
     target: {value: testMessage},
   });
 
-  fireEvent.click(screen.getByText("Start a game"));
-  expect(screen.getByLabelText("SMS Phone Number")).toHaveAttribute("aria-invalid", "false");
+  fireEvent.click(startGameLocator());
+  expect(phoneNumberLocator()).toHaveAttribute("aria-invalid", "false");
 });
 
 test("game name submit valid", async () => {
   const testMessage = 'Test Message';
   render(<StartGameInputs />)
-  expect(screen.getByLabelText("Your Game Name")).toHaveAttribute("aria-invalid", "false");
-  fireEvent.change(screen.getByLabelText("Your Game Name"), {
+  expect(gameNameLocator()).toHaveAttribute("aria-invalid", "false");
+  fireEvent.change(gameNameLocator(), {
     target: {value: testMessage},
   });
-  fireEvent.click(screen.getByText("Start a game"));
+  fireEvent.click(startGameLocator());
 
-  expect(screen.getByLabelText("Your Game Name")).toHaveAttribute("aria-invalid", "false");
+  expect(gameNameLocator()).toHaveAttribute("aria-invalid", "false");
 });
 
 test("game name submit invalid", async () => {
   render(<StartGameInputs />)
-  expect(screen.getByLabelText("Your Game Name")).toHaveAttribute("aria-invalid", "false");
-  fireEvent.click(screen.getByText("Start a game"));
+  expect(gameNameLocator()).toHaveAttribute("aria-invalid", "false");
+  fireEvent.click(startGameLocator());
 
-  expect(screen.getByLabelText("Your Game Name")).toHaveAttribute("aria-invalid", "true");
+  expect(gameNameLocator()).toHaveAttribute("aria-invalid", "true");
 });
 
 test("tiktok submit valid", async () => {
   const testMessage = 'Test Message';
   render(<StartGameInputs />)
-  expect(screen.getByLabelText("Your Game Name")).toHaveAttribute("aria-invalid", "false");
-  fireEvent.change(screen.getByLabelText("Tik Tok"), {
+  expect(gameNameLocator()).toHaveAttribute("aria-invalid", "false");
+  fireEvent.change(tikTokLocater(), {
     target: {value: testMessage},
   });
-  fireEvent.click(screen.getByText("Start a game"));
+  fireEvent.click(startGameLocator());
 
-  expect(screen.getByLabelText("Tik Tok")).toHaveAttribute("aria-invalid", "false");
+  expect(tikTokLocater()).toHaveAttribute("aria-invalid", "false");
 });
 
 test("tiktok submit invalid", async () => {
   render(<StartGameInputs />)
-  expect(screen.getByLabelText("Tik Tok")).toHaveAttribute("aria-invalid", "false");
-  fireEvent.click(screen.getByText("Start a game"));
+  expect(tikTokLocater()).toHaveAttribute("aria-invalid", "false");
+  fireEvent.click(startGameLocator());
 
-  expect(screen.getByLabelText("Tik Tok")).toHaveAttribute("aria-invalid", "true");
+  expect(tikTokLocater()).toHaveAttribute("aria-invalid", "true");
 });
