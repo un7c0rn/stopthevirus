4731a71c96711c486ef6ac9a02d107c9bc4695fd
dzx11 <dzx11@users.noreply.github.com>
2020-04-17 23:25:10 +0900

    Feature/join game page (#48)
    * Created join game page.

--- react_front_end/src/utilities/Utilities.js
+++ react_front_end/src/utilities/Utilities.js
@@ -1,3 +1,5 @@
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
 export const toCurrency = number => {
   // change to the locale en-US, de-DE
   const formatter = new Intl.NumberFormat("en-GB", {
@@ -6,6 +8,11 @@ export const toCurrency = number => {
   });
   const v = Number(number).toFixed(2);
   return formatter.format(v);
+};
+
+export const isSm = () => {
+  //check to see if user resolution is considered "small"
+  return useMediaQuery.bind(null, "(max-height:650px)")();//for iphone 5SE
 };
 
 // the next help function
