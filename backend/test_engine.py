bafbeaa3bed7254cab9713f2d5159041ac61053a
un7c0rn <brandon@formless.la>
2020-04-20 00:54:08 -0700

    Update game event processor to use formatted and localized SMS message strings

--- backend/test_engine.py
+++ backend/test_engine.py
@@ -19,7 +19,7 @@ import time
 from game_engine.common import GameOptions, GameSchedule, STV_I18N_TABLE
 
 _TEST_EVENT_WAIT_TIME = 10
-_TEST_AMAZON_SQS_CONFIG_PATH = '../amazon/stopthevirus-test.fifo.json'
+_TEST_AMAZON_SQS_CONFIG_PATH = '../amazon/stopthevirus.fifo.json'
 _TEST_TWILIO_SMS_CONFIG_PATH = '../twilio/stv-twilio.json'
 _TEST_FIRESTORE_INSTANCE_JSON_PATH = '../firebase/stv-game-db-test-4c0ec2310b2e.json'
 _TEST_GAME_ID = '7rPwCJaiSkxYgDocGDw1'
@@ -152,13 +152,13 @@ _TEST_EVENTS = [
 class EngineTest(unittest.TestCase):
 
     def setUp(self):
+        self._game_options = GameOptions()
+        self._game_options.game_schedule = STV_I18N_TABLE['US']
 
     @mock.patch('game_engine.twilio.TwilioSMSNotifier.send')
     def test_event_processing(self, send_fn):
         engine = Engine(
-            options=GameOptions(
-
-            ),
+            options=self._game_options,
             sqs_config_path=_TEST_AMAZON_SQS_CONFIG_PATH,
             twilio_config_path=_TEST_TWILIO_SMS_CONFIG_PATH,
             gamedb=_gamedb
