372b73db4aadb4148c27ec9a4aa80c6f5038ddb5
dzx11 <dzeifman2010@gmail.com>
2020-04-23 16:13:47 +0900

    Switch to Material-UI dark theme

--- react_front_end/src/App.js
+++ react_front_end/src/App.js
@@ -13,6 +13,9 @@ const GameInfoPage = lazy(() => import("./pages/GameInfoPage"));
 
 const theme = createMuiTheme({
   background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+  palette: {
+    type: 'dark',
+  },
 });
 
 export const AppContext = createContext();
