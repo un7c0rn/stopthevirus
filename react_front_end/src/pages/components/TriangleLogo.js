8923511dc89d0b3dd90a2cbbf7095c4ffcb73fe4
dzx11 <dzeifman2010@gmail.com>
2020-04-26 17:09:41 +0900

    Add "countdown" text around triangle logo

--- react_front_end/src/pages/components/TriangleLogo.js
+++ react_front_end/src/pages/components/TriangleLogo.js
@@ -3,15 +3,17 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import logo from "../../assets/vir-us_triangle_logo_white.jpeg";
+import Grid from "@material-ui/core/Grid";
+
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
     flexWrap: "wrap",
     backgroundColor:"black",
+    width: "100vw",//10 by 9 aspect ratio
+
     "& > *": {
-      width: "100vw",//10 by 9 aspect ratio
-      height: "10vh",
       display: "flex",
       justifyContent: "center",
       alignItems: "center"
@@ -20,16 +22,64 @@ const useStyles = makeStyles(theme => ({
   title: {
     margin: 0,
     fontSize: "4em"
+  },
+  text: {
+    margin:"0em 2em",
+    fontSize:"0.7em",
+    fontFamily: "Helvetica Neue",
+    fontWeight:"bold",
+  },
+  unit: {
+    margin:"0 2em",
+    fontSize:"0.5em",
+    fontFamily: "Helvetica Neue"
   }
 }));
 
 export default function TriangleLogo() {
   const classes = useStyles();
+  const dd=78, hh="06", mm=26, ss=47;
 
   return (
     <div className={classes.root}
     >
-      <img src={logo} style={{width:"30vw",height:"27vw", margin:"0 auto"}}/>
+    <img src={logo} style={{width:"30vw",height:"27vw", margin:"0 auto"}}/>
+
+    <Grid container justify="center">
+      <Grid item>
+        <span className={classes.text}>
+          {dd} <br/>
+        </span>
+        <span className={classes.unit}>
+           DAYS
+        </span>
+
+      </Grid>
+      <Grid item>
+        <span className={classes.text}>
+          {hh} <br/>
+        </span>
+        <span className={classes.unit}>
+           HOURS
+        </span>
+      </Grid>
+      <Grid item>
+        <span className={classes.text}>
+          {mm} <br/>
+        </span>
+        <span className={classes.unit}>
+           MINUTES
+        </span>
+      </Grid>
+      <Grid item>
+        <span className={classes.text}>
+          {ss} <br/>
+        </span>
+        <span className={classes.unit}>
+           SECONDS
+        </span>
+      </Grid>
+    </Grid>
     </div>
   );
 }
