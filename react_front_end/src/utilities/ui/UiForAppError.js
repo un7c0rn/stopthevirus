18dd425c2afc4bc7c1650818d9f9052d68830513
Bernard Baker <bernardbaker@users.noreply.github.com>
2020-05-03 19:23:51 +0100

    Updated custom error boundary UI component to display landing page content. (#102)

--- react_front_end/src/utilities/ui/UiForAppError.js
+++ react_front_end/src/utilities/ui/UiForAppError.js
@@ -1,16 +1,26 @@
-import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import AppTitle from "../../pages/components/AppTitle";
+import Footer from "../../pages/components/Footer";
+import StartGamePrompt from "../../pages/components/StartGamePrompt";
+import TriangleLogo from "../../pages/components/TriangleLogo";
+import LogoContainer from "../../pages/components/LogoContainer";
+import {
+  StatisticsLeft,
+  StatisticsRight,
+} from "../../pages/components/GameStatistics";
 
-export const UiForAppError = ({ error }) => {
+export default function UiForAppError({ error }) {
   const useStyles = makeStyles((theme) => ({
     root: {
       background: theme.background,
-      color: "black",
+      color: "white",
       height: "100vh",
       width: "100vw",
       display: "flex",
       flexDirection: "column",
       justifyContent: "flex-start",
+      marginTop: "4em",
     },
   }));
 
@@ -18,13 +28,17 @@ export const UiForAppError = ({ error }) => {
 
   return (
     <>
-      <section
-        className={classes.root}
-        id="foo"
-        data-testid="Game App Error Page"
-      >
-        {error.message}
+      <section className={classes.root} data-testid="Game App Error Page">
+        <AppTitle />
+        <StartGamePrompt />
+        <LogoContainer>
+          <StatisticsLeft />
+          <TriangleLogo />
+          <StatisticsRight />
+        </LogoContainer>
+        <h1>{error.message}</h1>
+        <Footer />
       </section>
     </>
   );
-};
+}
