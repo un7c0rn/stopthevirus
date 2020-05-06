3551c61d326ebbd92e0d19e4ff6a6ddc5e14518a
Bernard Baker <bernard.baker.me@gmail.com>
2020-05-06 14:53:43 +0100

    Updated URLs to be absolute paths.

--- react_front_end/src/services/TikTok.js
+++ react_front_end/src/services/TikTok.js
@@ -3,7 +3,14 @@ import fetch from "node-fetch";
 export const metricParser = async (url = null) => {
   try {
     if (!url) throw Error("TikTok URL not provided");
-    const response = await fetch(`/.netlify/functions/tiktok_video?url=${url}`);
+
+    const response = await fetch(
+      process.env?.REACT_DEVELOPMENT_ENV === "development"
+        ? "http://localhost:8888" +
+            `/.netlify/functions/tiktok_video?url=${url}`
+        : process.env?.WEBHOOK_REDIRECT_URL +
+            `/.netlify/functions/tiktok_video?url=${url}`
+    );
     const data = await response.json();
     return data.video;
   } catch (e) {
@@ -16,8 +23,13 @@ export const metricParser = async (url = null) => {
 export const getProfile = async (handle = null) => {
   try {
     if (!handle) throw Error("TikTok handle not provided");
+
     const response = await fetch(
-      `/.netlify/functions/tiktok_profile?handle=${handle}`
+      process.env?.REACT_DEVELOPMENT_ENV === "development"
+        ? "http://localhost:8888" +
+            `/.netlify/functions/tiktok_profile?handle=${handle}`
+        : process.env?.WEBHOOK_REDIRECT_URL +
+            `/.netlify/functions/tiktok_profile?handle=${handle}`
     );
     return await response.json();
   } catch (e) {
