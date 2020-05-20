66a6cac0cc6cf95ac0497ce8967bd1c4e4fb46b3
Bernard Baker <bernard.baker.me@gmail.com>
2020-05-20 13:53:42 +0100

    Updated footer component layout.

--- react_front_end/src/pages/common/Footer.js
+++ react_front_end/src/pages/common/Footer.js
@@ -1,48 +1,34 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import Button from "@material-ui/core/Button";
-import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 
 import "./Footer.scss";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    backgroundColor: "black",
-    "& > *": {
-      width: "100vw",
-      justifyContent: "space-between",
-      alignItems: "center",
-      padding: "1em 1em"
-    }
-  },
-  links: {
-    display: "flex",
-    flexDirection: "column"
-  },
-
-  text: {
-    textAlign:"justify",
-    fontSize:"0.6rem"
- }
-}));
-
 export default function Footer() {
-  const classes = useStyles();
-
-  const rules = "If the GLC, in its sole discretion, has authorized a Retailer to sell Tickets for On-Line Games at one or more of its Retailer Business Locations, in addition to all provisions, terms, and conditions of the Act, other Rules and Regulations, and the Retailer Contract, the On-Line Games Rules and Regulations herein shall apply to all On-Line Games. To the extent of any inconsistency with either the Retailer Contract or with the Retailer Rules and Regulations found in Chapter 2 of the GLC Policies and Procedures Manual, the On-Line Game Rules and Regulations shall govern the On-Line Games.";
-
-  const preventDefault = (event) => event.preventDefault();
-
   return (
-    <footer className={classes.root}>
+    <footer className="footer">
       <Paper square>
-        <Typography className={classes.text}>
-           <b> Rules:</b>{rules}
-
+        <Typography className="text ">
+          <b>
+            Rules: If the GLC, in its sole discretion, has authorized a Retailer
+            to sell Tickets for On-Line Games at one or more of its Retailer
+            Business Locations, in addition to all provisions, terms, and
+            conditions of the Act, other Rules and Regulations, and the Retailer
+            Contract, the On-Line Games Rules and Regulations herein shall apply
+            to all On-Line Games. To the extent of any inconsistency with either
+            the Retailer Contract or with the Retailer Rules and Regulations
+            found in Chapter 2 of the GLC Policies and Procedures Manual, the
+            On-Line Game Rules and Regulations shall govern the On-Line Games.
+          </b>
+          If the GLC, in its sole discretion, has authorized a Retailer to sell
+          Tickets for On-Line Games at one or more of its Retailer Business
+          Locations, in addition to all provisions, terms, and conditions of the
+          Act, other Rules and Regulations, and the Retailer Contract, the
+          On-Line Games Rules and Regulations herein shall apply to all On-Line
+          Games. To the extent of any inconsistency with either the Retailer
+          Contract or with the Retailer Rules and Regulations found in Chapter 2
+          of the GLC Policies and Procedures Manual, the On-Line Game Rules and
+          Regulations shall govern the On-Line Games.
         </Typography>
       </Paper>
     </footer>
