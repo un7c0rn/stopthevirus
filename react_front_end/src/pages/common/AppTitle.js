4bd5be18b85cad70f32502e4021d95d6e9167d8f
Bernard Baker <bernardbaker@users.noreply.github.com>
2020-05-23 16:35:59 +0100

    Animated the map of the globe. (#148)

--- react_front_end/src/pages/common/AppTitle.js
+++ react_front_end/src/pages/common/AppTitle.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import logo from "../../assets/vir-us.logo.type.white.letters.svg";
-import bg from "../../assets/vir-us_world_map_transparent_canvas.svg";
+import { ReactComponent as Bg } from "../../assets/vir-us_world_map_transparent_canvas.svg";
 import "./AppTitle.scss";
 import "./Effects.scss";
+
+const Map = () => {
+  return <Bg />;
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,9 +34,32 @@ const useStyles = makeStyles((theme) => ({
 export default function AppTitle() {
   const classes = useStyles();
 
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  useEffect(() => {
+    const list = document.querySelectorAll("svg g g use");
+    const iterate = () => {
+      const nodes = shuffle([...list]).slice(0, 1000);
+      while (nodes.length) {
+        const node = nodes.pop();
+        setTimeout(() => {
+          node.classList.add("effect-fill");
+        }, Math.random() * 2000);
+      }
+    };
+    list.length && iterate();
+  }, []);
+
   return (
     <div className={classes.root}>
-      <img src={bg} className={classes.background} />
+      {/* <img src={Bg} className={classes.background} /> */}
+      <Map className={classes.background} />
       <img src={logo} className="logo logo-animation" />
     </div>
   );
