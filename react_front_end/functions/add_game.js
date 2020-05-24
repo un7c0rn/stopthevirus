d4534051fc6b18176f3ca68405f5f14567529a91
Bernard Baker <bernard.baker.me@gmail.com>
2020-05-24 20:49:25 +0100

    Corrected the response JSON payload.

--- react_front_end/functions/add_game.js
+++ react_front_end/functions/add_game.js
@@ -13,6 +13,6 @@ exports.handler = async (event, context, callback) => {
 
     callback(null, { statusCode: 200, body: JSON.stringify(response) });
   } catch (err) {
-    callback(null, { statusCode: 500, body: { err } });
+    callback(null, { statusCode: 200, body: JSON.stringify({ error: err }) });
   }
 };
