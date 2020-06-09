c4c48b8e5c577403b5214e03ce6a4ea6e81fdb23
dzx11 <dzeifman2010@gmail.com>
2020-06-09 22:15:12 +0900

    Add start/stop and lock for thread

--- backend/matchmaker_service.py
+++ backend/matchmaker_service.py
@@ -15,6 +15,7 @@ class MatchmakerService:
         self._gamedb = FirestoreDB(json_config_path=json_config_path, game_id=123)
         self._region=region
         self._STOP = False
+        self._daemon_started = False
 
     def matchmaker_function(self, sleep_seconds=60):
         print("HELLO")
@@ -28,10 +29,19 @@ class MatchmakerService:
         print ("STOPPING")
 
     def start_matchmaker_daemon(self, sleep_seconds=60):
-        thread = threading.Thread(target=self.matchmaker_function, args=(sleep_seconds,))
-        thread.start()
+        if not self._daemon_started:
+            thread = threading.Thread(target=self.matchmaker_function, args=(sleep_seconds,))
+            self._daemon_started = True
+            thread.start()
+        else:
+            print("CAN ONLY RUN 1 DAEMON AT A TIME")
 
     def set_stop(self):
         print("TRYING TO STOP")
         self._STOP = True
+        self._daemon_started = False
+
+    def set_run(self):
+        self._STOP = False
+        print("STOP CONDITION REMOVED")
 
--- backend/test_matchmaker_service.py
+++ backend/test_matchmaker_service.py
@@ -17,9 +17,9 @@ class MatchmakerServiceTest(unittest.TestCase):
     def test_matchmaker_daemon(self):
         service = MatchmakerService()
         service.start_matchmaker_daemon(sleep_seconds=1)
-        #service.matchmaker_function(sleep_seconds=10)
-        #time.sleep(2)
+        time.sleep(2)
         service.set_stop()
+
         
         
 if __name__ == '__main__':
