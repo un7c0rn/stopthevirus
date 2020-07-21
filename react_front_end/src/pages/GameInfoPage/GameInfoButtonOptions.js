28c2d423e3689a2bd4077ec0dac22abd936792ff
Bernard Baker <bernardbaker@users.noreply.github.com>
2020-07-21 12:20:24 +0100

    Feature/build failure fixes (#163)
    * Fixed Netlify build CI warnings.

--- react_front_end/src/pages/GameInfoPage/GameInfoButtonOptions.js
+++ react_front_end/src/pages/GameInfoPage/GameInfoButtonOptions.js
@@ -1,5 +1,4 @@
 import Button from "@material-ui/core/Button";
-import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import { maxButtonWidth } from "../../utilities/Constants";
